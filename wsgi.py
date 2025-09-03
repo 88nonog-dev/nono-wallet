6389142a4656efeca329c07099f8521465d133af
88nonog-dev <88nonog@gmail.com>
2025-09-03 10:23:05 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,9 @@
-from app import application
+# نصدّر متغير اسمه app لأن startCommand يستخدم wsgi:app
+try:
+    from app import application as app
+except ImportError:
+    # احتياط إذا عندك متغير app داخل app.py
+    from app import app as app
+
+# نُبقي application أيضًا متاح (اختياري)
+application = app
