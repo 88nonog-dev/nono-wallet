3c54030b23e5b83e20d2fb5128c08eae32b99255
88nonog-dev <88nonog@gmail.com>
2025-09-11 12:54:54 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 from app import app, db
-=======
-﻿from app import app, db
->>>>>>> 044c657 (fix(wsgi): correct indentation and UTF-8 encoding)
 
-with app.app_context():
-    try:
-        db.create_all()
-    except Exception as e:
-        app.logger.warning(f"db.create_all warning: {e}")
+if __name__ == "__main__":
+    with app.app_context():
+        try:
+            db.create_all()
+        except Exception as e:
+            app.logger.warning(f"db.create_all warning: {e}")
